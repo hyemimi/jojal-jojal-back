07d7df17f3b4347f261b3761d43ae68a2f37f93a
hyemimi <hyemi7375@gmail.com>
2025-03-13 22:20:52 +0900

    refactor: 불필요한 파일 제거

--- Jojal-Jojal/src/main/java/community/Jojal_Jojal/repository/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package community.Jojal_Jojal.repository;
-import community.Jojal_Jojal.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
