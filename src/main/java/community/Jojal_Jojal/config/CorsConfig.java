2cb4c223d6da435554936357a96e2eaf8f540ae9
hyemimi <hyemi7375@gmail.com>
2025-03-21 16:48:39 +0900

    feat: 로그인 응답 에러 : dto, service, controller 수정

---
+++ src/main/java/community/Jojal_Jojal/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package community.Jojal_Jojal.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true); // 쿠키 허용
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500")); // 허용할 프론트엔드 주소
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type")); // 허용할 헤더
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
