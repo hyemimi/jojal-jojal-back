757275b10ce6aca6927da512e482f6c756f6f9f7
hyemimi <hyemi7375@gmail.com>
2025-03-27 13:08:57 +0900

    feat: 좋아요 삭제 수정

--- src/main/java/community/Jojal_Jojal/service/PostService.java
+++ src/main/java/community/Jojal_Jojal/service/PostService.java
@@ -97,9 +97,12 @@ public class PostService {
     public void deleteHeart(Long id) {
         Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
         Integer heart = post.getLike_count();
+
+        if (heart == 0) {
+            return;
+        }
         post.setLike_count(heart - 1);
         postRepository.save(post);
-        return;
     }
 
 
